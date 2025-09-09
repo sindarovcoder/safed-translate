890fd3da877117417a0ab746204b0a56300702a0
Safediy <sindarovsardor9@gmail.com>
2025-09-09 12:47:16 +0500

    add webhookewwef

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 try {
   require("dotenv").config();
-  // require('./services/messageHandler');
   require("./server");
-  // require("./bot")
 
-  //   process.on("uncaughtException", (err) => {
-  //     console.error(`There was an uncaught error: ${err.message}`);
-  //     console.error(err.stack);
-  //   });
+  process.on("unhandledRejection", (error) => {
+    console.error(`There was an unhandled rejection: ${error.message}`);
+    console.error(error.stack);
+  });
 
-  //   process.on("unhandledRejection", (err) => {
-  //     console.error(`There was an unhandleRejection error: ${err.message}`);
-  //     console.error(err.stack);
-  //   });
-} catch (err) {
-  console.error(`There was an uncaught error: ${err.message}`);
-  console.error(err.stack);
+  process.on("uncaughtException", (error) => {
+    console.error(`There was an uncaught error: ${error.message}`);
+    console.error(error.stack);
+  });
+} catch (error) {
+  console.error(`There was an uncaught error: ${error.message}`);
+  console.error(error.stack);
 }
