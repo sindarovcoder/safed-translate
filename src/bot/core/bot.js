6a83d7b13edd92d63e2193f0753a2eb5867c9393
Safediy <sindarovsardor9@gmail.com>
2025-09-09 12:02:26 +0500

    add webhook

--- src/bot/core/bot.js
+++ src/bot/core/bot.js
@@ -1,8 +1,8 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const bot = new TelegramBot(process.env.TOKEN, {
-  polling: true,
-  interval: 200,
-});
+const TOKEN = process.env.TOKEN;
+const NODE_ENV = process.env.NODE_ENV;
+
+const bot = new TelegramBot(TOKEN, { polling: !!NODE_ENV });
 
 module.exports = bot;
--- src/server.js
+++ src/server.js
@@ -1,50 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-// const bot = require("./services/botService");
-const bot = require("./bot");
-// const {b2cBot, p2cBot} = require('./services/botService');
-const { notification } = require("./webhooks/index.js");
-const { notificationValidation } = require("./validation/index.js");
+const bot = require("./bot/core/bot");
+
 const app = express();
-app.use(bodyParser.json());
-
-const { googleTranslateLanguages } = require("./base/languages");
-
-const token = process.env.TOKEN;
-// const P2C_TOKEN = process.env.P2C_TOKEN;
-
-// const webhookUrl = `${process.env.WEBHOOK_URL}/bot${token}`;
-// bot.setWebHook(webhookUrl);
-//
-
-// app.post(`/bot${P2C_TOKEN}`, (req, res) => {
-//   bot.processUpdate(req.body);
-//   res.sendStatus(200);
-// });
-//
-app.post(`/bot${token}`, (req, res) => {
-  // return 
+const TOKEN = process.env.BOT_TOKEN;
 
 
-  p2cBot.processUpdate(req.body);
+bot.setWebHook(`${process.env.RENDER_EXTERNAL_URL}/webhook/${TOKEN}`);
+app.post(`/webhook/${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
-app.get(`/bot/5142f97c-994b-48f9-984a-5bbf215e68d0/languages`, (req, res) => {
-  const languagesArray = Object.entries(googleTranslateLanguages).map(([code, lang]) => {
-    return `.${code}: ${Object.values(lang)[0]}`;
-  });
-
-  const responseText = languagesArray.join(",\n  "); // Har bir elementni yangi qatorda chiqarish
-
-  res.setHeader("Content-Type", "text/plain");
-  res.send(responseText);
+app.get('/ret-time', (_, res) => {
+  res.json({ time: new Date().toISOString() });
 });
-
-
-app.post(`/bot${token}/notification`, notificationValidation, notification);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Express server is listening on port ${PORT}`);
+  console.log(`🪄  Bot server is listening on port ${PORT}`);
 });
